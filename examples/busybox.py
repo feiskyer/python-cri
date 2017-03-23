b11ae83a339d28a384ade7422cf874cb850fc4a0
Pengfei Ni <feiskyer@gmail.com>
2017-03-23 17:01:33 +0800

    Add examples

--- examples/busybox.py
+++ examples/busybox.py
@@ -1,40 +1,5 @@
-# python-cri
-
-Python client for kubernetes container runtime interface (CRI).
-
-See [CRI](https://github.com/kubernetes/community/blob/master/contributors/design-proposals/runtime-client-server.md) for more details.
-
-## Install
-
-```sh
-pip install cri
-```
-
-## Usage
-
-```python
-import grpc
-from cri import api_pb2
-
-channel = grpc.insecure_channel('unix:///var/run/dockershim.sock')
-runtime_stub = api_pb2.RuntimeServiceStub(channel)
-image_stub = api_pb2.ImageServiceStub(channel)
-
-# Get runtime version
-print runtime_stub.Version(api_pb2.VersionRequest())
-
-# Get sandboxes
-print runtime_stub.ListPodSandbox(api_pb2.ListPodSandboxRequest())
-
-# Get Images
-print image_stub.ListImages(api_pb2.ListImagesRequest())
-```
-
-## Full example
-
-Create a busybox container in dockershim.
-
-```python
+#!/usr/bin/env python
+# Create a busybox container in dockershim.
 import grpc
 from cri import api_pb2
 
@@ -67,5 +32,6 @@ container_resp = runtime_stub.CreateContainer(api_pb2.CreateContainerRequest(
 
 print runtime_stub.StartContainer(api_pb2.StartContainerRequest(
     container_id=container_resp.container_id))
-```
 
+print runtime_stub.StopPodSandbox(
+    api_pb2.StopPodSandboxRequest(pod_sandbox_id=sandbox_resp.pod_sandbox_id))
